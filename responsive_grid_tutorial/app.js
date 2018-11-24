280fc6ca2b188461a37bf76813b69f05f928e063
screencast <screencast@Wilsonss-iMac.local>
2018-11-24 10:16:53 -0700

    Initial Commit

---
+++ responsive_grid_tutorial/app.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+  
+
+/* -----------------------------------------------
+                MOBILE NAVIGATION     
+-------------------------------------------------- */
+
+  $(".burger-icon").on("click", function() {
+	  if($( ".main-nav" ).hasClass( "mobile-hide" )){
+		    $(".main-nav").removeClass("mobile-hide");
+		    $(".main-nav").addClass("mobile-show");
+	  }else{
+		      $(".main-nav").addClass("mobile-hide");
+		      $(".main-nav").removeClass("mobile-show");
+	  }
+  
+  });
+
+
+});
+
+
+
